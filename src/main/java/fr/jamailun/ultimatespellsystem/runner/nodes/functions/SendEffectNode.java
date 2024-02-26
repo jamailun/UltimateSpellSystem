3b361da63f679a17f58d1ebb1209790b63d6ddd8
jamailun <=>
2024-02-26 23:40:29 +0100

    Finalized effect give node

--- src/main/java/fr/jamailun/ultimatespellsystem/runner/nodes/functions/SendEffectNode.java
+++ src/main/java/fr/jamailun/ultimatespellsystem/runner/nodes/functions/SendEffectNode.java
@@ -1,11 +1,13 @@
 package fr.jamailun.ultimatespellsystem.runner.nodes.functions;
 
+import fr.jamailun.ultimatespellsystem.UltimateSpellSystem;
 import fr.jamailun.ultimatespellsystem.dsl.nodes.type.Duration;
 import fr.jamailun.ultimatespellsystem.dsl.nodes.type.PotionEffect;
 import fr.jamailun.ultimatespellsystem.runner.RuntimeExpression;
 import fr.jamailun.ultimatespellsystem.runner.RuntimeStatement;
 import fr.jamailun.ultimatespellsystem.runner.SpellRuntime;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.potion.PotionEffectType;
 import org.jetbrains.annotations.Nullable;
 
 public class SendEffectNode extends RuntimeStatement {
@@ -27,13 +29,54 @@ public class SendEffectNode extends RuntimeStatement {
         LivingEntity target = runtime.safeEvaluate(targetRef, LivingEntity.class);
         PotionEffect effect = runtime.safeEvaluate(effectRef, PotionEffect.class);
         Duration duration = runtime.safeEvaluate(durationRef, Duration.class);
-        int durationSecs = (int) duration.toSeconds();
+        int durationTicks = (int) duration.toTicks();
         int power = 1;
         if(optPowerRef != null) {
             power = runtime.safeEvaluate(optPowerRef, Double.class).intValue();
+            if(power < 1) {
+                power = 1;
+                UltimateSpellSystem.logWarning("Invalid power value to effect " + effect);
+            }
         }
-        //TODO create potion effect :)
-        // getByName ....
-        target.addPotionEffect(new org.bukkit.potion.PotionEffect(null, durationSecs, power));
+        PotionEffectType bukkitEffect = convertEffect(effect);
+        target.addPotionEffect(new org.bukkit.potion.PotionEffect(bukkitEffect, durationTicks, power - 1));
+    }
+
+    private PotionEffectType convertEffect(PotionEffect effect) {
+        return switch (effect) {
+            case SPEED -> PotionEffectType.SPEED;
+            case SLOW -> PotionEffectType.SLOW;
+            case HASTE -> PotionEffectType.FAST_DIGGING;
+            case FATIGUE -> PotionEffectType.SLOW_DIGGING;
+            case STRENGTH -> PotionEffectType.INCREASE_DAMAGE;
+            case INSTANT_HEALTH -> PotionEffectType.HEAL;
+            case INSTANT_DAMAGE -> PotionEffectType.HARM;
+            case JUMP_BOOST -> PotionEffectType.JUMP;
+            case NAUSEA -> PotionEffectType.CONFUSION;
+            case REGENERATION -> PotionEffectType.REGENERATION;
+            case RESISTANCE -> PotionEffectType.DAMAGE_RESISTANCE;
+            case FIRE_RESISTANCE -> PotionEffectType.FIRE_RESISTANCE;
+            case WATER_BREATHING -> PotionEffectType.WATER_BREATHING;
+            case INVISIBILITY -> PotionEffectType.INVISIBILITY;
+            case BLINDNESS -> PotionEffectType.BLINDNESS;
+            case NIGHT_VISION -> PotionEffectType.NIGHT_VISION;
+            case HUNGER -> PotionEffectType.HUNGER;
+            case WEAKNESS -> PotionEffectType.WEAKNESS;
+            case POISON -> PotionEffectType.POISON;
+            case WITHER -> PotionEffectType.WITHER;
+            case HEALTH_BOOST -> PotionEffectType.HEALTH_BOOST;
+            case ABSORPTION -> PotionEffectType.ABSORPTION;
+            case SATURATION -> PotionEffectType.SATURATION;
+            case GLOWING -> PotionEffectType.GLOWING;
+            case LEVITATION -> PotionEffectType.LEVITATION;
+            case LUCK -> PotionEffectType.LUCK;
+            case BAD_LUCK -> PotionEffectType.UNLUCK;
+            case SLOW_FALLING -> PotionEffectType.SLOW_FALLING;
+            case CONDUIT_POWER -> PotionEffectType.CONDUIT_POWER;
+            case DOLPHIN_GRACE -> PotionEffectType.DOLPHINS_GRACE;
+            case BAD_OMEN -> PotionEffectType.BAD_OMEN;
+            case HERO_OF_THE_VILLAGE -> PotionEffectType.HERO_OF_THE_VILLAGE;
+            case DARKNESS -> PotionEffectType.DARKNESS;
+        };
     }
 }
