e9f9803e236d2b60c9bbd6e1e51fcef26dcc7492
timothe.rosaz <jamailun@Timordinateur>
2024-11-02 21:22:19 +0100

    Comparison will work now

--- src/main/java/fr/jamailun/ultimatespellsystem/bukkit/runner/nodes/operators/RunEqualsOrNotOpe.java
+++ src/main/java/fr/jamailun/ultimatespellsystem/bukkit/runner/nodes/operators/RunEqualsOrNotOpe.java
@@ -1,5 +1,6 @@
 package fr.jamailun.ultimatespellsystem.bukkit.runner.nodes.operators;
 
+import fr.jamailun.ultimatespellsystem.bukkit.UltimateSpellSystem;
 import fr.jamailun.ultimatespellsystem.bukkit.runner.RuntimeExpression;
 
 import java.util.Objects;
@@ -16,8 +17,17 @@ public final class RunEqualsOrNotOpe extends RuntimeBiOperators {
     @Override
     protected Boolean evaluate(Object left, Object right) {
         if(isEqual)
-            return Objects.equals(left, right);
-        return ! Objects.equals(left, right);
+            return equals(left, right);
+        return ! equals(left, right);
+    }
+
+    private boolean equals(Object left, Object right) {
+        if(left instanceof Number l && right instanceof Number r) {
+            UltimateSpellSystem.logDebug("Comparison (numbers). L="+l+"; R="+r);
+            return l.doubleValue() == r.doubleValue();
+        }
+        UltimateSpellSystem.logDebug("Comparison. L="+left+"|"+left.getClass()+"; R="+right+"|"+right.getClass());
+        return Objects.equals(left, right);
     }
 
     @Override
--- src/main/java/fr/jamailun/ultimatespellsystem/bukkit/runner/nodes/operators/RuntimeBiOperators.java
+++ src/main/java/fr/jamailun/ultimatespellsystem/bukkit/runner/nodes/operators/RuntimeBiOperators.java
@@ -3,6 +3,8 @@ package fr.jamailun.ultimatespellsystem.bukkit.runner.nodes.operators;
 import fr.jamailun.ultimatespellsystem.bukkit.runner.RuntimeExpression;
 import fr.jamailun.ultimatespellsystem.bukkit.runner.SpellRuntime;
 
+import java.util.List;
+
 public abstract class RuntimeBiOperators extends RuntimeExpression {
 
     protected final RuntimeExpression leftExpression;
@@ -15,7 +17,15 @@ public abstract class RuntimeBiOperators extends RuntimeExpression {
 
     @Override
     public final Object evaluate(SpellRuntime runtime) {
-        return evaluate(leftExpression.evaluate(runtime), rightExpression.evaluate(runtime));
+        Object left = leftExpression.evaluate(runtime);
+        if(left instanceof List<?> listLeft && listLeft.size() == 1)
+            left = listLeft.get(0);
+
+        Object right = leftExpression.evaluate(runtime);
+        if(right instanceof List<?> listRight && listRight.size() == 1)
+            right = listRight.get(0);
+
+        return evaluate(left, right);
     }
 
     protected abstract Object evaluate(Object left, Object right);
