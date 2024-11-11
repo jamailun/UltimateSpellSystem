6f2e0f3c518a26af4fb56a65a81b95fe7a2349ef
timothe.rosaz <jamailun@Timordinateur>
2024-11-11 13:19:56 +0100

    Forgot this file in previous commit

---
+++ src/main/java/fr/jamailun/ultimatespellsystem/bukkit/spells/AbstractSpell.java
@@ -0,0 +1,61 @@
+package fr.jamailun.ultimatespellsystem.bukkit.spells;
+
+import fr.jamailun.ultimatespellsystem.api.UltimateSpellSystem;
+import fr.jamailun.ultimatespellsystem.api.bukkit.spells.Spell;
+import fr.jamailun.ultimatespellsystem.api.bukkit.events.EntityCastSpellEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * A spell is something that can be done by a caster.
+ */
+public abstract class AbstractSpell implements Spell {
+
+    private final String name;
+    private boolean enabled = true;
+
+    public AbstractSpell(@NotNull String name) {
+        this.name = name;
+    }
+
+    @Override
+    public final boolean castNotCancellable(@NotNull LivingEntity entity) {
+        Bukkit.getServer().getPluginManager().callEvent(new EntityCastSpellEvent(entity, this, false));
+        return castSpell(entity);
+    }
+
+    @Override
+    public final boolean cast(@NotNull Player player) {
+        EntityCastSpellEvent event = new EntityCastSpellEvent(player, this, true);
+        Bukkit.getServer().getPluginManager().callEvent(event);
+        if (!event.isCancelled())
+            return castSpell(player);
+        return false;
+    }
+
+    /**
+     * Cast the spell.
+     * @param player the caster.
+     * @return {@code true} if the spell cast properly. If {@code false}, then the spell has been dropped.
+     */
+    protected abstract boolean castSpell(@NotNull LivingEntity player);
+
+    @Override
+    public final boolean isEnabled() {
+        return enabled;
+    }
+
+    @Override
+    public final void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+        UltimateSpellSystem.logInfo("Spell '" + name + "' has been " + (enabled?"enabled":"disabled") + ".");
+    }
+
+    @Override
+    public final @NotNull String getName() {
+        return name;
+    }
+
+}
