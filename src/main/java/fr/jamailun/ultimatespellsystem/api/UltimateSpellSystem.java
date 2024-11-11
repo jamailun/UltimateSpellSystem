c705844a68a3d2fddbd19c287a16556978e8987e
timothe.rosaz <jamailun@Timordinateur>
2024-11-11 22:32:28 +0100

    More comments to API entry-point

--- src/main/java/fr/jamailun/ultimatespellsystem/api/UltimateSpellSystem.java
+++ src/main/java/fr/jamailun/ultimatespellsystem/api/UltimateSpellSystem.java
@@ -6,6 +6,9 @@ import fr.jamailun.ultimatespellsystem.api.bukkit.spells.SpellsManager;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * Entry point of the <b>Ultimate Spells System</b> API.
+ */
 public final class UltimateSpellSystem {
 
     private UltimateSpellSystem() {}
@@ -17,30 +20,58 @@ public final class UltimateSpellSystem {
         UltimateSpellSystem.plugin = plugin;
     }
 
+    /**
+     * Log a DEBUG message. Will only be sent if debug mode is enabled in the configuration.
+     * @param message the message to print.
+     */
     public static void logDebug(@NotNull String message) {
         plugin.logDebug(message);
     }
 
+    /**
+     * Log a INFO message.
+     * @param message the message to print.
+     */
     public static void logInfo(@NotNull String message) {
         plugin.logInfo(message);
     }
 
+    /**
+     * Log a WARN message.
+     * @param message the message to print.
+     */
     public static void logWarning(@NotNull String message) {
         plugin.logWarning(message);
     }
 
+    /**
+     * Log a ERROR message.
+     * @param message the message to print.
+     */
     public static void logError(@NotNull String message) {
         plugin.logError(message);
     }
 
+    /**
+     * Get the spell manager singleton instance.
+     * @return the non-null reference.
+     */
     public static @NotNull SpellsManager getSpellsManager() {
         return plugin.getSpellsManager();
     }
 
+    /**
+     * Get the summons manager singleton instance.
+     * @return the non-null reference.
+     */
     public static @NotNull SummonsManager getSummonsManager() {
         return plugin.getSummonsManager();
     }
 
+    /**
+     * Get the item binder singleton instance.
+     * @return the non-null reference.
+     */
     public static @NotNull ItemBinder getItemBinder() {
         return plugin.getItemBinder();
     }
@@ -60,6 +91,9 @@ public final class UltimateSpellSystem {
         return plugin.runTaskRepeat(runnable, delay, period);
     }
 
+    /**
+     * Reload the configuration.
+     */
     public static void reloadConfiguration() {
         plugin.reloadConfiguration();
     }
