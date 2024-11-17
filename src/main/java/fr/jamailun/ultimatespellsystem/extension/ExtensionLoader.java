857e8f06d1f8e3cbf02809fad6bdd835e9f59961
timothe.rosaz <jamailun@Timordinateur>
2024-11-17 20:20:14 +0100

    Extension adds more entity types

--- src/main/java/fr/jamailun/ultimatespellsystem/extension/ExtensionLoader.java
+++ src/main/java/fr/jamailun/ultimatespellsystem/extension/ExtensionLoader.java
@@ -5,6 +5,7 @@ import fr.jamailun.ultimatespellsystem.bukkit.providers.JavaFunctionProvider;
 import fr.jamailun.ultimatespellsystem.extension.functions.CastSpellFunction;
 import fr.jamailun.ultimatespellsystem.extension.functions.DistanceFunction;
 import fr.jamailun.ultimatespellsystem.extension.functions.RayCastFunction;
+import fr.jamailun.ultimatespellsystem.extension.scopes.EntityTypesScopes;
 
 /**
  * Extension loader.
@@ -24,9 +25,13 @@ public final class ExtensionLoader {
         loaded = true;
         UltimateSpellSystem.logInfo("Loading extension.");
 
+        // Functions
         JavaFunctionProvider.instance().register(new RayCastFunction(), "raycast", "raycast_block");
         JavaFunctionProvider.instance().register(new CastSpellFunction(), "cast");
         JavaFunctionProvider.instance().register(new DistanceFunction(), "distance", "dist");
+
+        // Others
+        EntityTypesScopes.register();
 
         UltimateSpellSystem.logInfo("Loaded extension.");
     }
