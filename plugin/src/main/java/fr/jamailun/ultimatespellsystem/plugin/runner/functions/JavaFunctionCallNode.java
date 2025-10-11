3290a614d27e7a2d5d4b7ee389c7a10ec19b6e84
timothe.rosaz <jamailun@laposte.net>
2025-10-11 17:49:55 +0200

    Fixed a bug with functions returning non-wrapped entities

--- plugin/src/main/java/fr/jamailun/ultimatespellsystem/plugin/runner/functions/JavaFunctionCallNode.java
+++ plugin/src/main/java/fr/jamailun/ultimatespellsystem/plugin/runner/functions/JavaFunctionCallNode.java
@@ -1,11 +1,17 @@
 package fr.jamailun.ultimatespellsystem.plugin.runner.functions;
 
+import fr.jamailun.ultimatespellsystem.api.entities.SpellEntity;
 import fr.jamailun.ultimatespellsystem.api.runner.RuntimeExpression;
 import fr.jamailun.ultimatespellsystem.api.runner.SpellRuntime;
 import fr.jamailun.ultimatespellsystem.api.runner.functions.RunnableJavaFunction;
+import fr.jamailun.ultimatespellsystem.dsl.nodes.type.TypePrimitive;
+import fr.jamailun.ultimatespellsystem.plugin.entities.BukkitSpellEntity;
 import lombok.RequiredArgsConstructor;
+import org.bukkit.entity.Entity;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.Collection;
 import java.util.List;
 
 /**
@@ -19,6 +25,36 @@ public class JavaFunctionCallNode extends RuntimeExpression {
 
     @Override
     public Object evaluate(@NotNull SpellRuntime runtime) {
-        return function.compute(arguments, runtime);
+        Object value = function.compute(arguments, runtime);
+
+        // Special case for entities. We want to map them as spell entities !
+        if(function.getDslDefinition().returnedType().is(TypePrimitive.ENTITY)) {
+            if(value instanceof Collection<?> coll) {
+                if( ! function.getDslDefinition().returnedType().isCollection()) {
+                    throw new RuntimeException("In execution of function '" + function.getDslDefinition().id() + "', return type is " + function.getDslDefinition().returnedType() + ". But got an unexpected COLLECTION " + value);
+                }
+                return coll.stream()
+                        .map(this::mapEntity)
+                        .toList();
+            }
+            return mapEntity(value);
+        }
+
+        return value;
+    }
+
+    private @Nullable SpellEntity mapEntity(@Nullable Object value) {
+        return switch (value) {
+            // Null ? We handle it.
+            case null -> null;
+
+            // Already a spell-entity.
+            case SpellEntity se -> se;
+
+            // Bukkit entity : we wrap it.
+            case Entity bukkitEntity -> new BukkitSpellEntity(bukkitEntity);
+
+            default -> throw new RuntimeException("In execution of function '" + function.getDslDefinition().id() + "', return type is " + function.getDslDefinition().returnedType() + ". But got: " + value + " of type" + value.getClass());
+        };
     }
 }
